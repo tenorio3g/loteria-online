9fa505dc16888397e75058d8178d626c51e8a25a
Fortunato Tenorio <tenorio.3g@hotmail.com>
2025-07-07 17:05:25 -0600

    Add files via upload

--- server.js
+++ server.js
@@ -5,71 +5,127 @@ const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
 app.use(express.static(__dirname));
+app.use(express.json());
 
-const TOTAL_NUMBERS = 54;
+const fs = require('fs');
+
 let players = [];
 let cards = {};
 let drawnNumbers = [];
 let gameStarted = false;
+let tipoJuego = "normal";
 
 function generateCard() {
-  const nums = [];
-  while (nums.length < 16) {
-    const rand = Math.floor(Math.random() * TOTAL_NUMBERS) + 1;
-    if (!nums.includes(rand)) nums.push(rand);
+  const numbers = [];
+  while (numbers.length < 16) {
+    const num = Math.floor(Math.random() * 54) + 1;
+    if (!numbers.includes(num)) numbers.push(num);
   }
-  return nums;
+  return numbers;
 }
 
-function drawNumber() {
-  if (drawnNumbers.length >= TOTAL_NUMBERS) return null;
-  let num;
-  do {
-    num = Math.floor(Math.random() * TOTAL_NUMBERS) + 1;
-  } while (drawnNumbers.includes(num));
-  drawnNumbers.push(num);
-  return num;
+function checkWin(card, marked, tipo) {
+  const index = (i, j) => i * 4 + j;
+
+  const isMarked = i => marked.includes(card[i]);
+
+  if (tipo === "llena") {
+    return card.every(n => marked.includes(n));
+  }
+
+  // Horizontal
+  for (let i = 0; i < 4; i++) {
+    if ([0, 1, 2, 3].every(j => isMarked(index(i, j)))) return true;
+  }
+
+  // Vertical
+  for (let j = 0; j < 4; j++) {
+    if ([0, 1, 2, 3].every(i => isMarked(index(i, j)))) return true;
+  }
+
+  // Diagonales
+  if ([0, 1, 2, 3].every(i => isMarked(index(i, i)))) return true;
+  if ([0, 1, 2, 3].every(i => isMarked(index(i, 3 - i)))) return true;
+
+  // Cuadro chico (centro 2x2)
+  const smallSquare = [5, 6, 9, 10];
+  if (smallSquare.every(i => isMarked(i))) return true;
+
+  // Cuadro grande (centro 3x3)
+  const bigSquare = [1, 2, 5, 6, 9, 10, 13, 14];
+  if (bigSquare.every(i => isMarked(i))) return true;
+
+  return false;
 }
 
-function checkWinner(card) {
-  return card.every(n => drawnNumbers.includes(n));
-}
-
-io.on('connection', (socket) => {
-  socket.on('join', (name) => {
-    players.push(name);
-    cards[socket.id] = generateCard();
-    socket.emit('card', cards[socket.id]);
-    io.emit('players', players);
-    io.emit('ready', players.length >= 4);
-  });
-
-  socket.on('iniciar', () => {
-    if (players.length >= 4 && !gameStarted) {
-      gameStarted = true;
-      const interval = setInterval(() => {
-        const num = drawNumber();
-        if (num === null) return clearInterval(interval);
-        io.emit('numberDrawn', num);
-        for (let id in cards) {
-          if (checkWinner(cards[id])) {
-            io.emit('winner', players[Object.keys(cards).indexOf(id)]);
-            clearInterval(interval);
-            break;
-          }
-        }
-      }, 3000);
-    }
-  });
-
-  socket.on('disconnect', () => {
-    players.splice(Object.keys(cards).indexOf(socket.id), 1);
-    delete cards[socket.id];
-    io.emit('players', players);
-    io.emit('ready', players.length >= 4);
+app.get('/cards.json', (req, res) => {
+  fs.readFile('./cards.json', (err, data) => {
+    if (err) return res.status(500).send("Error loading cards.json");
+    res.json(JSON.parse(data));
   });
 });
 
-http.listen(3000, () => {
+io.on('connection', (socket) => {
+  socket.on('join', (name) => {
+    if (!players.find(p => p.id === socket.id)) {
+      players.push({ id: socket.id, name, marked: [] });
+      cards[socket.id] = generateCard();
+    }
+
+    io.emit('players', players.map(p => p.name));
+    socket.emit('card', cards[socket.id]);
+
+    if (players.length >= 4 && !gameStarted) {
+      io.emit('ready', true);
+    }
+  });
+
+  socket.on('iniciar', (tipo) => {
+    tipoJuego = tipo;
+    gameStarted = true;
+    drawnNumbers = [];
+    io.emit('message', '¡Juego iniciado!');
+    drawNumber();
+  });
+
+  socket.on('disconnect', () => {
+    players = players.filter(p => p.id !== socket.id);
+    delete cards[socket.id];
+    io.emit('players', players.map(p => p.name));
+    if (players.length < 4) io.emit('ready', false);
+  });
+});
+
+function drawNumber() {
+  if (drawnNumbers.length >= 54) return;
+
+  let num;
+  do {
+    num = Math.floor(Math.random() * 54) + 1;
+  } while (drawnNumbers.includes(num));
+
+  drawnNumbers.push(num);
+  io.emit('numberDrawn', num);
+
+  let winner = null;
+  for (let p of players) {
+    const card = cards[p.id];
+    const marked = card.filter(n => drawnNumbers.includes(n));
+    if (checkWin(card, marked, tipoJuego)) {
+      winner = p.name;
+      break;
+    }
+  }
+
+  if (winner) {
+    io.emit('winner', winner);
+    gameStarted = false;
+    return;
+  }
+
+  setTimeout(drawNumber, 3000);
+}
+
+http.listen(process.env.PORT || 3000, () => {
   console.log('Servidor en puerto 3000');
 });
